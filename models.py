a706027e7406c95facd8d8450a3d95779a5903e2
YZoZ <mail@yzoz.com>
2023-01-07 16:09:21 +0300

    new models

--- models.py
+++ models.py
@@ -1,26 +1,84 @@
 from typing import Optional
 from pydantic import BaseModel, PositiveInt
 
-class Tx(BaseModel):
-    credit: str
-    debit: str
-    amount: PositiveInt
-    uniq: str
-    sign: str
-    type: Optional[int] = 0
-    msg: Optional[str] = ''
-    time: Optional[int]
-    hash: Optional[str]
-
-class UserContent(BaseModel):
-    addr: str
-    req: str
-    time: int
-    content: str
-    sign: str
+# Things
 
 class User(BaseModel):
     addr: str
     name: Optional[str]
     cover: Optional[str]
     desc: Optional[str]
+    sign: str
+class NFT(BaseModel):
+    creater: str
+    img: str
+    title: str
+    desc: str
+    uniq: str
+    sign: str
+    hash: Optional[str] #from sign
+    time: Optional[int]
+
+class Ask(BaseModel):
+    addr: str
+    amount: PositiveInt
+    uniq: str
+    title: Optional[str]
+    cover: Optional[str]
+    desc: Optional[str]
+    sign: str
+    prev_hash: Optional[str]
+    hash: Optional[str]
+    msg: Optional[str] = ''
+    time: Optional[int]
+
+class Fut(BaseModel):
+    creater: str
+    amount: PositiveInt
+    exp: int
+    uniq: str
+    sign: str
+    hash: Optional[str] #from sign
+    time: Optional[int]
+
+class Opt(BaseModel):
+    creater: str
+    amount: PositiveInt
+    exp: int
+    uniq: str
+    sign: str
+    hash: Optional[str] #from sign
+    time: Optional[int]
+
+
+# Actions
+class Tx(BaseModel):
+    credit: str
+    debit: str
+    amount: PositiveInt
+    uniq: str
+    sign: str
+    prev_hash: Optional[str]
+    hash: Optional[str]
+    msg: Optional[str] = ''
+    time: Optional[int]
+
+class Move(BaseModel):
+    credit: str
+    debit: str
+    type: int
+    id: str
+    uniq: str
+    sign: str
+    prev_hash: Optional[str]
+    hash: Optional[str]
+    time: Optional[int]
+
+class Vote(BaseModel):
+    addr: str
+    id: str
+    uniq: str
+    sign: str
+    prev_hash: Optional[str]
+    hash: Optional[str]
+    time: Optional[int]
